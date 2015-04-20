de55fe992e1f50a239f4ef629806f2e8be11ba79
Andy Aylward <aaylward@gmail.com>
2015-04-19 23:45:07 -0400

    clean and whatnot

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp');
 var nib = require('nib');
 var stylus = require('gulp-stylus');
+var del = require('del');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+
+gulp.task('clean', function () {
+  del(['dist/**']);
+});
 
 gulp.task('css', function(){
   gulp.src('./src/stylus/style.styl')
     .pipe(stylus({use: nib(), import: ['nib']}))
     .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('html', function(){
+  gulp.src('./src/html/index.html')
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', function() {
@@ -23,4 +33,4 @@ gulp.task('js', function() {
   .pipe(gulp.dest('dist/js'));
 });
  
-gulp.task('default', ['css', 'js']);
+gulp.task('default', ['clean', 'html', 'css', 'js']);
