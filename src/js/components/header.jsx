f466a5c132748baebd1799c1f9e5173b7a65bf3f
Andy Aylward <aaylward@gmail.com>
2015-05-04 01:18:16 -0400

    add instructions

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -4,7 +4,8 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className='board-header'>
-        <span>{`SCORE: ${this.props.score}`}</span>
+        <span className='score'>{`SCORE: ${this.props.score}`}</span>
+        <span className='instructions'>{'z - move left, x - move right, < spacebar > - shoot'}</span>
       </div>
     )
   }
